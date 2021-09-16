cf17202153f16e5318efd0da9241e03187148ade
ngtrainings <89327844+ngtrainings@users.noreply.github.com>
2021-09-16 09:59:36 +0530

    Update HomeController.java

--- springboot-helloworld-master/src/main/java/com/nxtgen/helloworld/controller/HomeController.java
+++ springboot-helloworld-master/src/main/java/com/nxtgen/helloworld/controller/HomeController.java
@@ -3,12 +3,29 @@ package com.nxtgen.helloworld.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from NxtGen! --> V2";
+        
+        try {
+            // Local address
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            String host = InetAddress.getLocalHost().getHostName();
+
+            // Remote address
+            String rip = InetAddress.getLoopbackAddress().getHostAddress();
+            String rhost = InetAddress.getLoopbackAddress().getHostName();
+            System.out.printf("Instance details :- %s:%d", ip, port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        
+        return "Hello from NextGen! --> V3. Local Server ip --> "+ip+ " Local Server Name --> "+host+" Remote Server ip --> "+rip+ " Remote Server Name --> "+rhost;
     }
 
 }

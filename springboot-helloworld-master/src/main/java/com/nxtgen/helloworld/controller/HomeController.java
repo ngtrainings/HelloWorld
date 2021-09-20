8a14fa4b7e74cbbcc26131d2291e8141af5d95cf
ngtrainings <89327844+ngtrainings@users.noreply.github.com>
2021-09-20 22:48:26 +0530

    Update HomeController.java

--- springboot-helloworld-master/src/main/java/com/nxtgen/helloworld/controller/HomeController.java
+++ springboot-helloworld-master/src/main/java/com/nxtgen/helloworld/controller/HomeController.java
@@ -8,6 +8,30 @@ import java.net.UnknownHostException;
 
 @RestController
 public class HomeController {
+    
+    @RequestMapping("/ingress")
+    String ingress() {
+        String message =  "";
+        
+        try {
+            // Local address
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            String host = InetAddress.getLocalHost().getHostName();
+
+            // Remote address
+            String rip = InetAddress.getLoopbackAddress().getHostAddress();
+            String rhost = InetAddress.getLoopbackAddress().getHostName();
+            
+            message = "Hello from NextGen! --> V6 --> ingress. Local Server ip --> "+ip+ " Local Server Name --> "+host+" Remote Server ip --> "+rip+ " Remote Server Name --> "+rhost;
+            
+            System.out.println(message);
+            
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        
+        return message;
+    }
 
     @RequestMapping("/home")
     String home() {
@@ -22,7 +46,7 @@ public class HomeController {
             String rip = InetAddress.getLoopbackAddress().getHostAddress();
             String rhost = InetAddress.getLoopbackAddress().getHostName();
             
-            message = "Hello from NextGen! --> V5 --> home. Local Server ip --> "+ip+ " Local Server Name --> "+host+" Remote Server ip --> "+rip+ " Remote Server Name --> "+rhost;
+            message = "Hello from NextGen! --> V6 --> home. Local Server ip --> "+ip+ " Local Server Name --> "+host+" Remote Server ip --> "+rip+ " Remote Server Name --> "+rhost;
             
             System.out.println(message);
             
